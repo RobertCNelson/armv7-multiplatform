94b5a262a2c616ec28ecd2062886537d0ea72499
Robert Nelson <robertcnelson@gmail.com>
2013-07-01 19:44:32 -0500

    3.10-armv7-x2 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -22,8 +22,8 @@ linaro_toolchain="cortex_gcc_4_8"
 
 #Kernel/Build
 KERNEL_REL=3.10
-KERNEL_TAG=${KERNEL_REL}-rc7
-BUILD=armv7-x1.4
+KERNEL_TAG=${KERNEL_REL}
+BUILD=armv7-x2
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
