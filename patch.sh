7233b2f92e1a2bf802a9f59837d78e824cade499
Robert Nelson <robertcnelson@gmail.com>
2013-07-22 11:41:24 -0500

    saucy: fix build error
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -205,7 +205,7 @@ dts () {
 saucy () {
 	echo "dir: saucy"
 	#Ubuntu Saucy: so Ubuntu decided to enable almost every Warning -> Error option...
-	${git} "${DIR}/patches/saucy/0001-saucy-disable-Werror-unused-but-set-variable.patch"
+	${git} "${DIR}/patches/saucy/0001-saucy-error-variable-ilace-set-but-not-used-Werror-u.patch"
 }
 
 arm
