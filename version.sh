c29846fc96f63c7ea7c48f4daaa2e980c464f8cd
Robert Nelson <robertcnelson@gmail.com>
2013-11-24 18:31:12 -0600

    3.13-rc1-armv7-x0 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,15 +25,15 @@ config="multi_v7_defconfig"
 linaro_toolchain="cortex_gcc_4_8"
 
 #Kernel/Build
-KERNEL_REL=3.12
-KERNEL_TAG=${KERNEL_REL}.1
-BUILD=armv7-x9
+KERNEL_REL=3.13
+KERNEL_TAG=${KERNEL_REL}-rc1
+BUILD=armv7-x0
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
 
 #git branch
-BRANCH="v3.12.x"
+BRANCH="v3.13.x"
 
 BUILDREV=1.0
 DISTRO=cross
