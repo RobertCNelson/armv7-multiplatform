41b824c7c6d7aa80e847e28f5bfea66c1f24d499
Robert Nelson <robertcnelson@gmail.com>
2013-11-25 08:20:22 -0600

    saucy: enable ilace fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -183,8 +183,8 @@ saucy () {
 	#need to be re-tested with v3.13-rcX
 	#Ubuntu Saucy: so Ubuntu decided to enable almost every Warning -> Error option...
 	${git} "${DIR}/patches/saucy/0001-saucy-error-variable-ilace-set-but-not-used-Werror-u.patch"
-	${git} "${DIR}/patches/saucy/0002-saucy-disable-Werror-pointer-sign.patch"
-	${git} "${DIR}/patches/saucy/0003-saucy-disable-stack-protector.patch"
+	#${git} "${DIR}/patches/saucy/0002-saucy-disable-Werror-pointer-sign.patch"
+	#${git} "${DIR}/patches/saucy/0003-saucy-disable-stack-protector.patch"
 }
 
 arm
@@ -204,6 +204,6 @@ omap3_beagle_xm_rework
 omap_sprz319_erratum
 fixes
 
-#saucy
+saucy
 
 echo "patch.sh ran successful"
