4486557fbae3c7b79a0166c710506c71c606cec4
Robert Nelson <robertcnelson@gmail.com>
2014-02-03 15:03:49 -0600

    saucy: still needs -Werror=pointer-sign fix
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -358,9 +358,10 @@ saucy () {
 	echo "dir: saucy"
 	#need to be re-tested with v3.14-rcX
 	#Ubuntu Saucy: so Ubuntu decided to enable almost every Warning -> Error option...
-	${git} "${DIR}/patches/saucy/0001-saucy-error-variable-ilace-set-but-not-used-Werror-u.patch"
-	${git} "${DIR}/patches/saucy/0002-saucy-disable-Werror-pointer-sign.patch"
-	${git} "${DIR}/patches/saucy/0003-saucy-disable-stack-protector.patch"
+	${git} "${DIR}/patches/saucy/0001-saucy-disable-Werror-pointer-sign.patch"
+
+	#${git} "${DIR}/patches/saucy/0001-saucy-error-variable-ilace-set-but-not-used-Werror-u.patch"
+	#${git} "${DIR}/patches/saucy/0003-saucy-disable-stack-protector.patch"
 }
 
 #revert
@@ -380,6 +381,6 @@ omap3_beagle_xm_rework
 
 fixes
 vivante
-#saucy
+saucy
 
 echo "patch.sh ran successful"
--- version.sh
+++ version.sh
@@ -29,7 +29,7 @@ toolchain="gcc_linaro_gnueabihf_4_8"
 #Kernel/Build
 KERNEL_REL=3.14
 KERNEL_TAG=${KERNEL_REL}-rc1
-BUILD=armv7-x0.2
+BUILD=armv7-x0.3
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
