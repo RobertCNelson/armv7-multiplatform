80a7f809f6045e3f6144df60955ccc82677ed55a
Robert Nelson <robertcnelson@gmail.com>
2014-02-03 15:26:56 -0600

    saucy: add fix for error: variable ‘ilace’ set but not used [-Werror=unused-but-set-variable]
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -359,8 +359,7 @@ saucy () {
 	#need to be re-tested with v3.14-rcX
 	#Ubuntu Saucy: so Ubuntu decided to enable almost every Warning -> Error option...
 	${git} "${DIR}/patches/saucy/0001-saucy-disable-Werror-pointer-sign.patch"
-
-	#${git} "${DIR}/patches/saucy/0001-saucy-error-variable-ilace-set-but-not-used-Werror-u.patch"
+	${git} "${DIR}/patches/saucy/0002-saucy-error-variable-ilace-set-but-not-used-Werror-u.patch"
 	#${git} "${DIR}/patches/saucy/0003-saucy-disable-stack-protector.patch"
 }
 
