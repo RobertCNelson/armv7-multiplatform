6dc276d7aad9eba2f8614a49f2e6b1b686cfbd00
Robert Nelson <robertcnelson@gmail.com>
2014-02-19 09:46:16 -0600

    beagle-xm: enable spidev by default
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -320,8 +320,7 @@ omap3_beagle_xm_rework () {
 	${git} "${DIR}/patches/omap3_beagle_xm_rework/0002-ARM-dts-omap3-beagle-xm-ab-usb-host-is-active-high-t.patch"
 	${git} "${DIR}/patches/omap3_beagle_xm_rework/0003-ARM-dts-beagle-xm-make-sure-dvi-is-enabled.patch"
 
-	#TESTING
-	#${git} "${DIR}/patches/omap3_beagle_xm_rework/0004-ARM-dts-beagle-xm-enable-spidev.patch"
+	${git} "${DIR}/patches/omap3_beagle_xm_rework/0004-ARM-dts-beagle-xm-enable-spidev.patch"
 }
 
 omap_sprz319_erratum () {
--- version.sh
+++ version.sh
@@ -29,7 +29,7 @@ toolchain="cortex_gcc_4_8"
 #Kernel/Build
 KERNEL_REL=3.13
 KERNEL_TAG=${KERNEL_REL}.3
-BUILD=armv7-x10
+BUILD=armv7-x10.1
 
 #v3.X-rcX + upto SHA
 #KERNEL_SHA=""
