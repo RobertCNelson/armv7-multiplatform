b1da1c49dff4ff9caad520a2c469a33d4c9b54d5
Robert Nelson <robertcnelson@gmail.com>
2014-07-14 15:59:22 -0500

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -9,12 +9,10 @@ else
 fi
 
 #Debian 7 (Wheezy): git version 1.7.10.4 and later needs "--no-edit"
-unset GIT_OPTS
-unset GIT_NOEDIT
-LC_ALL=C git help pull | grep -m 1 -e "--no-edit" >/dev/null 2>&1 && GIT_NOEDIT=1
-
-if [ "${GIT_NOEDIT}" ] ; then
-	GIT_OPTS="${GIT_OPTS} --no-edit"
+unset git_opts
+git_no_edit=$(LC_ALL=C git help pull | grep -m 1 -e "--no-edit" || true)
+if [ ! "x${git_no_edit}" = "x" ] ; then
+	git_opts="--no-edit"
 fi
 
 config="multi_v7_defconfig"
@@ -37,6 +35,6 @@ BUILD=armv7-x8
 #git branch
 BRANCH="v3.14.x"
 
-BUILDREV=1.0
 DISTRO=cross
 DEBARCH=armhf
+#
