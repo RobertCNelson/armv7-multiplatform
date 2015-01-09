bc92869c2dcc5a52eab5c5f6e642c94014d8cf1b
Robert Nelson <robertcnelson@gmail.com>
2015-01-09 13:15:03 -0600

    kernel bump: v3.18.2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -136,9 +136,10 @@ dts () {
 	${git} "${DIR}/patches/dts/0009-ARM-dts-omap3-beagle-ddc-i2c-bus-is-not-responding-d.patch"
 	${git} "${DIR}/patches/dts/0010-imx-add-udoo.patch"
 	${git} "${DIR}/patches/dts/0011-ARM-sun7i-add-support-for-A20-OLinuXino-Lime2.patch"
+	${git} "${DIR}/patches/dts/0009-ARM-dts-imx51-babbage-Fix-ULPI-PHY-reset-modelling.patch"
 
 	if [ "x${regenerate}" = "xenable" ] ; then
-		number=11
+		number=12
 		cleanup
 	fi
 }
