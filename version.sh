5155c00025c790aec2a190d4b7c26800b5b8929f
Robert Nelson <robertcnelson@gmail.com>
2015-04-29 09:15:50 -0500

    4.0.1-armv7-x2 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -13,8 +13,8 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 
 #Kernel/Build
 KERNEL_REL=4.0
-KERNEL_TAG=${KERNEL_REL}
-BUILD=armv7-x1.3
+KERNEL_TAG=${KERNEL_REL}.1
+BUILD=armv7-x2
 
 #v3.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
