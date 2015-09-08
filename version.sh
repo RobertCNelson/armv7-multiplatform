760cc328243376603786506d9de01d4cd6befa38
Robert Nelson <robertcnelson@gmail.com>
2015-09-08 17:10:12 -0500

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -2,19 +2,6 @@
 #
 ARCH=$(uname -m)
 
-if [ $(which nproc) ] ; then
-	CORES=$(nproc)
-else
-	CORES=1
-fi
-
-#Debian 7 (Wheezy): git version 1.7.10.4 and later needs "--no-edit"
-unset git_opts
-git_no_edit=$(LC_ALL=C git help pull | grep -m 1 -e "--no-edit" || true)
-if [ ! "x${git_no_edit}" = "x" ] ; then
-	git_opts="--no-edit"
-fi
-
 config="multi_v7_defconfig"
 
 #toolchain="gcc_linaro_eabi_4_8"
@@ -30,6 +17,7 @@ KERNEL_TAG=${KERNEL_REL}.5
 BUILD=armv7-x4
 
 #v3.X-rcX + upto SHA
+#prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
 #git branch
