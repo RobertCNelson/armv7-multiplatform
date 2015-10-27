4f9d77a3360fcc41c0aa3ca6232de6eebe44629f
Robert Nelson <robertcnelson@gmail.com>
2015-10-27 12:19:47 -0500

    rt: needs a rebase for v4.1.12
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -16,7 +16,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.11
-BUILD=armv7-rt-x6
+BUILD=armv7-rt-x6.1
 kernel_rt=".10-rt10"
 
 #v4.X-rcX + upto SHA
