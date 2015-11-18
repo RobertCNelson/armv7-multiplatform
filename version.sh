0388d84b56f09a4f4d28b4850e84bcb0d97aad6a
Robert Nelson <robertcnelson@gmail.com>
2015-11-18 10:18:11 -0600

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -4,6 +4,8 @@ ARCH=$(uname -m)
 
 config="multi_v7_defconfig"
 
+#arm
+KERNEL_ARCH=arm
 #toolchain="gcc_linaro_eabi_4_8"
 #toolchain="gcc_linaro_eabi_4_9"
 #toolchain="gcc_linaro_eabi_5"
@@ -12,12 +14,15 @@ config="multi_v7_defconfig"
 #toolchain="gcc_linaro_gnueabihf_4_8"
 toolchain="gcc_linaro_gnueabihf_4_9"
 #toolchain="gcc_linaro_gnueabihf_5"
+#arm64
+#KERNEL_ARCH=arm64
+#toolchain="gcc_linaro_aarch64_gnu_5"
 
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.13
 BUILD=armv7-x6
-kernel_rt=".12-rt13"
+kernel_rt=".13-rt15"
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
