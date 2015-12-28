f607462c1c6f8be9bbc4b84501832f44fed47af3
Robert Nelson <robertcnelson@gmail.com>
2015-12-27 21:32:05 -0600

    4.4-rc7-armv7-x3 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -20,8 +20,8 @@ toolchain="gcc_linaro_gnueabihf_5"
 
 #Kernel/Build
 KERNEL_REL=4.4
-KERNEL_TAG=${KERNEL_REL}-rc6
-BUILD=armv7-x2.1
+KERNEL_TAG=${KERNEL_REL}-rc7
+BUILD=armv7-x3
 kernel_rt=".X-rtY"
 
 #v4.X-rcX + upto SHA
