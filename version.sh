2607e0118341ead01332ad35d0c57e0ad03506b2
Robert Nelson <robertcnelson@gmail.com>
2016-05-04 20:49:42 -0500

    4.4.9-armv7-rt-x7 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_5"
 #Kernel/Build
 KERNEL_REL=4.4
 KERNEL_TAG=${KERNEL_REL}.9
-BUILD=${build_prefix}6.1
+BUILD=${build_prefix}7
 kernel_rt=".7-rt16"
 
 #v4.X-rcX + upto SHA
