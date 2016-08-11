93edc9ae78364d5ddec731633c415bc2257ec465
Robert Nelson <robertcnelson@gmail.com>
2016-08-11 15:22:33 -0500

    4.1.30-armv7-rt-x8 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}.30
-BUILD=${build_prefix}7
+BUILD=${build_prefix}8
 kernel_rt=".27-rt31"
 
 #v4.X-rcX + upto SHA
