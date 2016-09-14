f6d43ea235543eb2faf4ec65f2de29573e5f9e22
Robert Nelson <robertcnelson@gmail.com>
2016-09-13 21:05:50 -0500

    xm: fix reboot lockup
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,7 +28,7 @@ toolchain="gcc_linaro_gnueabihf_6"
 #Kernel/Build
 KERNEL_REL=4.8
 KERNEL_TAG=${KERNEL_REL}-rc6
-BUILD=${build_prefix}1
+BUILD=${build_prefix}1.1
 kernel_rt=""
 
 #v4.X-rcX + upto SHA
