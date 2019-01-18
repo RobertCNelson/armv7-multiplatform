ad14a4b7044074bcfa52ea5c0e6d0c52ab46d20b
Robert Nelson <robertcnelson@gmail.com>
2019-01-18 11:58:49 -0600

    fix: udoo
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -28,13 +28,14 @@ toolchain="gcc_linaro_gnueabihf_7"
 #toolchain="gcc_linaro_aarch64_gnu_5"
 #toolchain="gcc_linaro_aarch64_gnu_6"
 #toolchain="gcc_linaro_aarch64_gnu_7"
+#toolchain="gcc_arm_aarch64_gnu_8"
 
 #Kernel
 KERNEL_REL=4.15
 KERNEL_TAG=${KERNEL_REL}.17
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}1.2
+BUILD=${build_prefix}1.3
 
 #v4.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
