3fef78792757f3800404ec8e96732db7694a60ad
Robert Nelson <robertcnelson@gmail.com>
2021-03-04 11:27:42 -0600

    kernel v5.12-rc1 rebase with: device-tree/etc BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/eb25f2800649020dc1537f2e2f5c0f6e977344d6 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=6a849767df85ce9399494f53fb5c753665396653 WPANUSB: https://github.com/statropy/wpanusb/commit/7ba5f3d24d95f804e80b2d8d28e35b34c15219c2
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -8,7 +8,7 @@ build_prefix="-armv7-x"
 branch_prefix="v"
 branch_postfix=".x"
 
-#https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/tree/Documentation/process/changes.rst?h=v5.11-rc1
+#https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/tree/Documentation/process/changes.rst?h=v5.12-rc1
 #arm
 KERNEL_ARCH=arm
 DEBARCH=armhf
@@ -45,7 +45,7 @@ KERNEL_REL=5.12
 KERNEL_TAG=${KERNEL_REL}-rc1
 kernel_rt=".x-rty"
 #Kernel Build
-BUILD=${build_prefix}0
+BUILD=${build_prefix}0.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
