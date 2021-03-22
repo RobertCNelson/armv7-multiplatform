6b594b1d165d59ba00fe511705bc848aede8e984
Robert Nelson <robertcnelson@gmail.com>
2021-03-22 11:04:06 -0500

    kernel v5.9.16 rebase with: can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/8beef0cdfb1c0e3cb066e3678cb224445b2e436f BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/75ecfde4898095948de5ab72f87cf59759b474ba CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/b312dbd5e720833cb69bc65af3670a52dd1e93ca TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -103,6 +103,90 @@ external_git () {
 	${git_bin} describe
 }
 
+aufs_fail () {
+	echo "aufs failed"
+	exit 2
+}
+
+aufs () {
+	aufs_prefix="aufs5-"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		KERNEL_REL=5.9
+		wget https://raw.githubusercontent.com/sfjro/${aufs_prefix}standalone/aufs${KERNEL_REL}/${aufs_prefix}kbuild.patch
+		patch -p1 < ${aufs_prefix}kbuild.patch || aufs_fail
+		rm -rf ${aufs_prefix}kbuild.patch
+		${git_bin} add .
+		${git_bin} commit -a -m 'merge: aufs-kbuild' -s
+
+		wget https://raw.githubusercontent.com/sfjro/${aufs_prefix}standalone/aufs${KERNEL_REL}/${aufs_prefix}base.patch
+		patch -p1 < ${aufs_prefix}base.patch || aufs_fail
+		rm -rf ${aufs_prefix}base.patch
+		${git_bin} add .
+		${git_bin} commit -a -m 'merge: aufs-base' -s
+
+		wget https://raw.githubusercontent.com/sfjro/${aufs_prefix}standalone/aufs${KERNEL_REL}/${aufs_prefix}mmap.patch
+		patch -p1 < ${aufs_prefix}mmap.patch || aufs_fail
+		rm -rf ${aufs_prefix}mmap.patch
+		${git_bin} add .
+		${git_bin} commit -a -m 'merge: aufs-mmap' -s
+
+		wget https://raw.githubusercontent.com/sfjro/${aufs_prefix}standalone/aufs${KERNEL_REL}/${aufs_prefix}standalone.patch
+		patch -p1 < ${aufs_prefix}standalone.patch || aufs_fail
+		rm -rf ${aufs_prefix}standalone.patch
+		${git_bin} add .
+		${git_bin} commit -a -m 'merge: aufs-standalone' -s
+
+		${git_bin} format-patch -4 -o ../patches/aufs/
+
+		cd ../
+		if [ ! -d ./${aufs_prefix}standalone ] ; then
+			${git_bin} clone -b aufs${KERNEL_REL} https://github.com/sfjro/${aufs_prefix}standalone --depth=1
+			cd ./${aufs_prefix}standalone/
+				aufs_hash=$(git rev-parse HEAD)
+			cd -
+		else
+			rm -rf ./${aufs_prefix}standalone || true
+			${git_bin} clone -b aufs${KERNEL_REL} https://github.com/sfjro/${aufs_prefix}standalone --depth=1
+			cd ./${aufs_prefix}standalone/
+				aufs_hash=$(git rev-parse HEAD)
+			cd -
+		fi
+		cd ./KERNEL/
+		KERNEL_REL=5.9
+
+		cp -v ../${aufs_prefix}standalone/Documentation/ABI/testing/*aufs ./Documentation/ABI/testing/
+		mkdir -p ./Documentation/filesystems/aufs/
+		cp -rv ../${aufs_prefix}standalone/Documentation/filesystems/aufs/* ./Documentation/filesystems/aufs/
+		mkdir -p ./fs/aufs/
+		cp -v ../${aufs_prefix}standalone/fs/aufs/* ./fs/aufs/
+		cp -v ../${aufs_prefix}standalone/include/uapi/linux/aufs_type.h ./include/uapi/linux/
+
+		${git_bin} add .
+		${git_bin} commit -a -m 'merge: aufs' -m "https://github.com/sfjro/${aufs_prefix}standalone/commit/${aufs_hash}" -s
+		${git_bin} format-patch -5 -o ../patches/aufs/
+		echo "AUFS: https://github.com/sfjro/${aufs_prefix}standalone/commit/${aufs_hash}" > ../patches/git/AUFS
+
+		rm -rf ../${aufs_prefix}standalone/ || true
+
+		${git_bin} reset --hard HEAD~5
+
+		start_cleanup
+
+		${git} "${DIR}/patches/aufs/0001-merge-aufs-kbuild.patch"
+		${git} "${DIR}/patches/aufs/0002-merge-aufs-base.patch"
+		${git} "${DIR}/patches/aufs/0003-merge-aufs-mmap.patch"
+		${git} "${DIR}/patches/aufs/0004-merge-aufs-standalone.patch"
+		${git} "${DIR}/patches/aufs/0005-merge-aufs.patch"
+
+		wdir="aufs"
+		number=5
+		cleanup
+	fi
+
+	dir 'aufs'
+}
+
 can_isotp () {
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
@@ -219,19 +303,19 @@ rt () {
 }
 
 ti_pm_firmware () {
-	#http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=shortlog;h=refs/heads/ti-v4.1.y-next
+	#https://git.ti.com/gitweb?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=shortlog;h=refs/heads/ti-v4.1.y
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
 
 		cd ../
 		if [ ! -d ./ti-amx3-cm3-pm-firmware ] ; then
-			${git_bin} clone -b ti-v4.1.y-next git://git.ti.com/processor-firmware/ti-amx3-cm3-pm-firmware.git --depth=1
+			${git_bin} clone -b ti-v4.1.y git://git.ti.com/processor-firmware/ti-amx3-cm3-pm-firmware.git --depth=1
 			cd ./ti-amx3-cm3-pm-firmware
 				ti_amx3_cm3_hash=$(git rev-parse HEAD)
 			cd -
 		else
 			rm -rf ./ti-amx3-cm3-pm-firmware || true
-			${git_bin} clone -b ti-v4.1.y-next git://git.ti.com/processor-firmware/ti-amx3-cm3-pm-firmware.git --depth=1
+			${git_bin} clone -b ti-v4.1.y git://git.ti.com/processor-firmware/ti-amx3-cm3-pm-firmware.git --depth=1
 			cd ./ti-amx3-cm3-pm-firmware
 				ti_amx3_cm3_hash=$(git rev-parse HEAD)
 			cd -
@@ -266,6 +350,10 @@ dtb_makefile_append_omap4 () {
 	sed -i -e 's:omap4-panda.dtb \\:omap4-panda.dtb \\\n\t'$device' \\:g' arch/arm/boot/dts/Makefile
 }
 
+dtb_makefile_append_am5 () {
+	sed -i -e 's:am57xx-beagle-x15.dtb \\:am57xx-beagle-x15.dtb \\\n\t'$device' \\:g' arch/arm/boot/dts/Makefile
+}
+
 dtb_makefile_append () {
 	sed -i -e 's:am335x-boneblack.dtb \\:am335x-boneblack.dtb \\\n\t'$device' \\:g' arch/arm/boot/dts/Makefile
 }
@@ -298,13 +386,15 @@ beagleboard_dtbs () {
 		device="omap4-panda-es-b3.dtb" ; dtb_makefile_append_omap4
 
 		#device="am335x-abbbi.dtb" ; dtb_makefile_append
-		#device="am335x-bonegreen-gateway.dtb" ; dtb_makefile_append
+		device="am335x-bonegreen-gateway.dtb" ; dtb_makefile_append
 
-		#device="am335x-boneblack-uboot.dtb" ; dtb_makefile_append
+		device="am335x-boneblack-uboot.dtb" ; dtb_makefile_append
+		device="am335x-sancloud-bbe-uboot.dtb" ; dtb_makefile_append
 
 		#device="am335x-bone-uboot-univ.dtb" ; dtb_makefile_append
 		#device="am335x-boneblack-uboot-univ.dtb" ; dtb_makefile_append
 		#device="am335x-bonegreen-wireless-uboot-univ.dtb" ; dtb_makefile_append
+		#device="am335x-sancloud-bbe-uboot-univ.dtb" ; dtb_makefile_append
 
 		${git_bin} add -f arch/arm/boot/dts/
 		${git_bin} add -f include/dt-bindings/
@@ -334,6 +424,7 @@ local_patch () {
 }
 
 #external_git
+aufs
 can_isotp
 wpanusb
 #rt
@@ -380,7 +471,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.11-rc5"
+	backport_tag="v5.11.8"
 
 	subsystem="wlcore"
 	#regenerate="enable"
@@ -396,7 +487,22 @@ backports () {
 	fi
 
 	${git} "${DIR}/patches/backports/wlcore/0002-wlcore-Downgrade-exceeded-max-RX-BA-sessions-to-debu.patch"
-	${git} "${DIR}/patches/backports/wlcore/0003-wlcore-Fix-command-execute-failure-19-for-wl12xx.patch"
+
+	backport_tag="v5.10.25"
+
+	subsystem="greybus"
+	#regenerate="enable"
+	if [ "x${regenerate}" = "xenable" ] ; then
+		pre_backports
+
+		cp -rv ~/linux-src/drivers/greybus/* ./drivers/greybus/
+		cp -rv ~/linux-src/drivers/staging/greybus/* ./drivers/staging/greybus/
+
+		post_backports
+		exit 2
+	else
+		patch_backports
+	fi
 }
 
 reverts () {
@@ -419,8 +525,10 @@ reverts () {
 }
 
 drivers () {
+	#exit 2
 	dir 'RPi'
 	dir 'drivers/ar1021_i2c'
+	dir 'drivers/sound'
 	dir 'drivers/spi'
 	dir 'drivers/tps65217'
 
@@ -454,9 +562,9 @@ drivers
 soc
 
 packaging () {
-	#do_backport="enable"
+	do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.6-rc4"
+		backport_tag="v5.10.25"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
