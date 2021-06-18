8652e1e3c360ac92cfddcc6629d92892ae9f9c21
Robert Nelson <robertcnelson@gmail.com>
2021-06-18 17:47:49 -0500

    kernel v5.8.18 rebase with: can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/c3286f2ea36d198035e1038e7310c6edb537535a BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/7f2f67f468b1c31710609eb8c74ecc5a0f5eedf2 CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/593c79fb0f4d6e36f1cce5d1c880e26664bc574e TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=04f3e5411b1cdc59292775e7b8e05a5d35ea4a80
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -421,7 +421,6 @@ beagleboard_dtbs () {
 		cp -vr ../${work_dir}/src/arm/* arch/arm/boot/dts/
 		cp -vr ../${work_dir}/include/dt-bindings/* ./include/dt-bindings/
 
-		device="am335x-abbbi.dtb" ; dtb_makefile_append
 		device="am335x-bonegreen-gateway.dtb" ; dtb_makefile_append
 
 		device="am335x-boneblack-uboot.dtb" ; dtb_makefile_append
@@ -504,7 +503,7 @@ patch_backports (){
 }
 
 backports () {
-	backport_tag="v5.12.7"
+	backport_tag="v5.12.12"
 
 	subsystem="greybus"
 	#regenerate="enable"
@@ -520,7 +519,7 @@ backports () {
 		patch_backports
 	fi
 
-	backport_tag="v5.12.7"
+	backport_tag="v5.12.12"
 
 	subsystem="wlcore"
 	#regenerate="enable"
@@ -700,9 +699,9 @@ backports () {
 		${git_bin} commit -a -m "cypress fmac patchset" -m "v5.4.18-2021_0114" -s
 		${git_bin} format-patch -1 -o ../patches/cypress/
 
-		exit 2
+	#	exit 2
 	#else
-	#	patch_backports
+		patch_backports
 	fi
 
 	#dir 'cypress'
@@ -736,12 +735,15 @@ drivers () {
 	dir 'drivers/sound'
 	dir 'drivers/spi'
 	dir 'drivers/tps65217'
+	dir 'drivers/mmc'
 
 	dir 'drivers/ti/cpsw'
 	dir 'drivers/ti/serial'
 	dir 'drivers/ti/tsc'
 	dir 'drivers/ti/gpio'
+	dir 'drivers/ti/mmc'
 	dir 'drivers/greybus'
+	dir 'drivers/bluetooth'
 #	dir 'drivers/mikrobus'
 	dir 'drivers/serdev'
 	dir 'drivers/iio'
@@ -767,7 +769,7 @@ soc
 packaging () {
 	do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.10.40"
+		backport_tag="v5.10.45"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
