e3ffa04cf6adb58654a1d6a4a7b37eb19c9877b5
Robert Nelson <robertcnelson@gmail.com>
2021-09-16 11:18:21 -0500

    5.4.143-armv7-rt-x58.1 release AUFS: https://github.com/sfjro/aufs5-standalone/commit/995f07ca91f8e03f4686a36d1abd433d764a57ab BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/c56312f1e0ed990ba2844e38e4a5fa080af53487 CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/beb4650660179963a8ed5b5cbf2085cc1b34f608 RT: patch-5.4.143-rt64.patch.xz TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WIREGUARD: https://git.zx2c4.com/WireGuard/commit/edad0d6e99e5133b1e8e865d727a25fff6399cb4 WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=47007d0169c4c6c855a3fc5333c142489a43e89e
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -816,7 +816,7 @@ backports () {
 
 	dir 'cypress'
 
-	backport_tag="v5.4.146"
+	backport_tag="v5.4.147"
 
 	subsystem="iio"
 	#regenerate="enable"
@@ -891,7 +891,7 @@ soc
 packaging () {
 	do_backport="enable"
 	if [ "x${do_backport}" = "xenable" ] ; then
-		backport_tag="v5.10.65"
+		backport_tag="v5.10.66"
 
 		subsystem="bindeb-pkg"
 		#regenerate="enable"
