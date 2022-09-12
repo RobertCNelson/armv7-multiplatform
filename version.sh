6e15362e966d92e7a75fdd1d3d1065030fe977b1
Robert Nelson <robertcnelson@gmail.com>
2022-09-12 18:47:53 -0500

    kernel v5.4.209 rebase with rt: v5.4.209-rt77 wireguard/can_isotp/device-tree/etc AUFS: https://github.com/sfjro/aufs5-standalone/commit/995f07ca91f8e03f4686a36d1abd433d764a57ab BBDTBS: https://github.com/beagleboard/BeagleBoard-DeviceTrees/commit/a88ed1070035414888c0fbd3e062308bf032210e CAN-ISOTP: https://github.com/hartkopp/can-isotp/commit/7626d0a0707391970080d493ce69638719938da7 RT: patch-5.4.209-rt77.patch.xz TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c WIREGUARD: https://git.zx2c4.com/WireGuard/commit/edad0d6e99e5133b1e8e865d727a25fff6399cb4 WPANUSB: https://github.com/statropy/wpanusb/commit/251f0167545bf2dcaa3cad991a59dbf5ab05490a BCFSERIAL: https://github.com/statropy/bcfserial/commit/aded88429a8a00143596b41f4c1f50d9ae3d4069 WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=9dc9c89375c996c4d1d1a8c0a76f9dd04e244077
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -18,6 +18,7 @@ toolchain="gcc_8_arm"
 #toolchain="gcc_9_arm"
 #toolchain="gcc_10_arm"
 #toolchain="gcc_11_arm"
+#toolchain="gcc_12_arm"
 #arm64
 #KERNEL_ARCH=arm64
 #DEBARCH=arm64
@@ -27,6 +28,7 @@ toolchain="gcc_8_arm"
 #toolchain="gcc_9_aarch64"
 #toolchain="gcc_10_aarch64"
 #toolchain="gcc_11_aarch64"
+#toolchain="gcc_12_aarch64"
 #riscv64
 #KERNEL_ARCH=riscv
 #DEBARCH=riscv64
@@ -35,15 +37,16 @@ toolchain="gcc_8_arm"
 #toolchain="gcc_9_riscv64"
 #toolchain="gcc_10_riscv64"
 #toolchain="gcc_11_riscv64"
+#toolchain="gcc_12_riscv64"
 
 #Kernel
 KERNEL_REL=5.4
-KERNEL_TAG=${KERNEL_REL}.182
-kernel_rt=".182-rt70"
+KERNEL_TAG=${KERNEL_REL}.209
+kernel_rt=".209-rt77"
 #Kernel Build
-BUILD=${build_prefix}64
+BUILD=${build_prefix}64.1
 
-#v5.X-rcX + upto SHA
+#v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
 #KERNEL_SHA=""
 
