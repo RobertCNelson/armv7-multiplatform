bf44944f459d70b87e399c7703bc796c65cc5533
Robert Nelson <robertcnelson@gmail.com>
2024-04-16 15:19:06 -0500

    kernel v6.6.25-armv7-rt-x12.1 rebase external git projects and rt: v6.6.25-rt29 BBDTBS: https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees/-/commit/9022e0c2cda86bb195f30f0affc29f40b6b018ac RT: patch-6.6.25-rt29.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=73529a882f83afa26402b18680025334b0215d92 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -242,10 +242,6 @@ arm_dtbo_makefile_append () {
 	cp -v ../${work_dir}/src/arm/overlays/${device}.dts arch/arm/boot/dts/ti/omap/${device}.dtso
 }
 
-k3_dtb_makefile_append () {
-	echo "dtb-\$(CONFIG_ARCH_K3) += $device" >> arch/arm64/boot/dts/ti/Makefile
-}
-
 beagleboard_dtbs () {
 	branch="v6.6.x"
 	https_repo="https://openbeagle.org/beagleboard/BeagleBoard-DeviceTrees.git"
@@ -388,7 +384,7 @@ patch_backports () {
 }
 
 backports () {
-	backport_tag="v5.10.213"
+	backport_tag="v5.10.215"
 
 	subsystem="uio"
 	#regenerate="enable"
@@ -423,13 +419,12 @@ backports () {
 }
 
 drivers () {
-#	dir 'boris'
-	dir 'drivers/sdhci-omap'
+	dir 'boris'
 }
 
 ###
 backports
-drivers
+#drivers
 
 packaging () {
 	echo "Update: package scripts"
