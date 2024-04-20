384b9771c948e64397bd4b1c89ad9fe62a49af1d
Robert Nelson <robertcnelson@gmail.com>
2024-04-20 12:54:53 -0500

    kernel v6.1.83-armv7-rt-x8.1 rebase external git projects and rt: v6.1.83-rt28 RT: patch-6.1.83-rt28.patch.xz WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/commit/?id=73529a882f83afa26402b18680025334b0215d92 TI_AMX3_CM3: http://git.ti.com/gitweb/?p=processor-firmware/ti-amx3-cm3-pm-firmware.git;a=commit;h=fb484c5e54f2e31cf0a338d2927a06a2870bcc2c
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- patch.sh
+++ patch.sh
@@ -502,14 +502,12 @@ patch_backports () {
 }
 
 backports () {
-	backport_tag="v5.10.213"
-
 	subsystem="uio"
 	#regenerate="enable"
 	if [ "x${regenerate}" = "xenable" ] ; then
-		pre_backports
+		unset backport_tag
 
-		cp -v ~/linux-src/drivers/uio/uio_pruss.c ./drivers/uio/
+		cp -v ../patches/drivers/ti/uio/uio_pruss.c ./drivers/uio/
 
 		post_backports
 		exit 2
@@ -544,6 +542,7 @@ drivers () {
 
 fixes () {
 	dir 'drivers/sdhci-omap'
+	dir 'mmc'
 }
 
 ###
