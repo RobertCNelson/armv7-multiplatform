ad76f735de6fcd75049701ab6df5332757178d7f
Robert Nelson <robertcnelson@gmail.com>
2025-10-29 09:38:35 -0500

    scripts: resync all build scripts
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ scripts/pull.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git pull --no-edit https://github.com/RobertCNelson/armv7-multiplatform.git v6.1.x
+git pull --no-edit https://gitlab.com/RobertCNelson/armv7-multiplatform.git v6.1.x
+
