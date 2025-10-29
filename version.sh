f2c7283135c5760439db217fa1618809f6ad79fa
Robert Nelson <robertcnelson@gmail.com>
2025-10-29 11:55:14 -0500

    6.1.157-armv7-x16 release WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/wens/wireless-regdb.git/tag/?h=master-2025-10-07
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -58,7 +58,7 @@ KERNEL_TAG=${KERNEL_REL}.157
 #https://mirrors.edge.kernel.org/pub/linux/kernel/projects/rt/6.1/
 kernel_rt=".157-rt57"
 #Kernel Build
-BUILD=${build_prefix}15.1
+BUILD=${build_prefix}16
 
 #v6.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
